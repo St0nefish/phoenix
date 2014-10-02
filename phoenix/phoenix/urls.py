f81a06bd0e5a20126000998e0f0d3d84d1d76d74
St0nefish <logan.gagne@gmail.com>
2014-10-01 23:28:02 -0400

    Collected static files to make project self sufficient

--- phoenix/phoenix/urls.py
+++ phoenix/phoenix/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'phoenix.views.home', name='home'),
@@ -9,3 +12,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^test/', 'testPage.views.test', name='test'),
 )
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
