8bb315d338d8cb9efa3fb69c8ad143f28d5609e7
St0nefish <logan.gagne@gmail.com>
2014-10-03 08:32:23 -0400

    gitignore, forgot self

--- phoenix/users/models.py
+++ phoenix/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Player(models.Model):
+    userid = models.OneToOneField(User)
+    birthday = models.DateField()
+    name = models.CharField(max_length=100)
+    
+    def __unicode__(self):
+        return self.name 
+
+# Create user object to attach to player object
+def create_player_user_callback(sender, instance, **kwargs):
+    player, new = Player.objects.get_or_create(user=instance)
+post_save.connect(create_player_user_callback, User)
